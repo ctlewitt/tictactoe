866af304614b873332299d2fd43115af63f9769d
Charley Lewittes <ctlewitt@gmail.com>
2015-12-30 13:52:36 -0500

    fixed get_move() working on check_for_winner(player) have thought of more efficientn way to evaluate winner

--- two_player.py
+++ two_player.py
@@ -14,6 +14,7 @@ XES = "X"
 OHS = "O"
 board = []
 winner = ""
+draw = False
 
 def fill_board():
     for row in range(0,3):
@@ -52,37 +53,81 @@ def get_move(player):
     valid_first_moves = ['A', 'B', 'C'] #REPLACE WITH NUMBER
     valid_second_moves = ['1', '2', '3']
     while not valid_move:
-        move = input(player,"'s move (enter coordinates like battleship. e.g., B3): ")
+        move = raw_input(player + "'s move (enter coordinates like battleship. e.g., B3): ")
         #check_move
         if len(move) == 2:
             if (valid_first_moves.__contains__(move[0])) and (valid_second_moves.__contains__(move[1])):
                 first_move = convert_to_move(move[0])
-                second_move = move[1] - 1
+                second_move = int(move[1]) - 1
                 if board[first_move][second_move] == EMPTY:
                     valid_move = True
                     board[first_move][second_move] = player
-       else:
-            print "Invalid Move"
+                else:
+                    print "Invalid Move: please select an unoccupied space"
+            else:
+                print "Invalid Move: must be A-C followed by 1-3"
+        else:
+            print "Invalid Move: please indicate 2 coordinates"
 
+#checks for winner and draw at same time
 def check_for_winner(player):
+    temp_player = ""
+    temp_player_wins = False
     #check rows
     for row in range(0,3):
-        
+        temp_player = board[row][0]
+        temp_player_wins = True
+        for col in range(0,3):
+            if temp_player != board[row][col]:
+                temp_player_wins = False
+        if temp_player_wins:
+            winner = temp_player
+            return
+
     #check cols
+    for col in range(0,3):
+        temp_player = board[0][col]
+        temp_player_wins = True
+        for row in range(0,3):
+            if temp_player != board[row][col]:
+                temp_player_wins = False
+        if temp_player_wins:
+            winner = temp_player
+            return
+
     #check diagonals
+    temp_player1 = board[0][0]
+    temp_player2 = board[0][2]
+    temp_player1_wins = True
+    temp_player2_wins = True
+    for index in range(0,3):
+
+    pass
 
 def congratulate_winner(player):
     print "Congratulations,", player, " you won!"
 
+def draw():
+    for row in range(0,3):
+        for col in range(0,3):
+            if board[row][col] != "":
+                return False
+    return True
+
 fill_board()
 print "Welcome to TicTacToe"
 player = XES
-while winner == "":
+while winner == "" and not draw():
     if player == XES:
         player = OHS
     else:
         player = XES
     print_board()
-    get_move()
+    print
+    get_move(player)
     check_for_winner(player)
-congratulate_winner(player)
+print_board()
+if winner != "":
+    congratulate_winner(player)
+else: #tie
+    print "Tie!  Play again soon!"
