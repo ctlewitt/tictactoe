2bb7b501d0ac3930d49711c3fdb5b317d4eab8f5
Charley Lewittes <ctlewitt@gmail.com>
2016-01-26 03:13:34 -0500

    fixed game type  & board size selection bug

--- two_player.py
+++ two_player.py
@@ -238,7 +238,7 @@ class TicTacToe:
         while True:
             game_type = raw_input("Enter 1 to play against computer or 2 to play in two player mode: ")
             if game_type == "1" or game_type == "2":
-                return game_type
+                return int(game_type)
             else:
                 print "You must select 1 or 2."
 
