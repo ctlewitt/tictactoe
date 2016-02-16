0449cf1544fa7e10f55817f4a5fe6bba15a771e7
Charley Lewittes <ctlewitt@gmail.com>
2016-02-16 06:20:39 -0500

    moved constants into class

--- runner.py
+++ runner.py
@@ -1,18 +1,19 @@
-import two_player
+from two_player import TicTacToe
 
 # run game:
 print "Welcome to TicTacToe"
 # get mode, size, first player as appropriate and set up game
-game_type = two_player.TicTacToe.get_game_type()
-if game_type == two_player.TWO_PLAYER_MODE:
-    size = two_player.TicTacToe.get_size()
-    my_game = two_player.TicTacToe(game_type, size)
+game_type = TicTacToe.get_game_type()
+if game_type == TicTacToe.TWO_PLAYER_MODE:
+    size = TicTacToe.get_size()
+    my_game = TicTacToe(game_type, size)
 else:
-    player_xo_choice = two_player.TicTacToe.get_player_xo_choice()
-    my_game = two_player.TicTacToe(game_type, two_player.MIN_BOARD, player_xo_choice)
+    difficulty = TicTacToe.get_difficulty_choice()
+    player_xo_choice = TicTacToe.get_player_xo_choice()
+    my_game = TicTacToe(game_type, TicTacToe.MIN_BOARD, player_xo_choice, difficulty)
 
 # play until someone wins or quits or there is a draw
-while my_game.winner == two_player.EMPTY and not my_game.draw() and not my_game.quit:
+while my_game.winner == TicTacToe.EMPTY and not my_game.draw() and not my_game.quit:
     my_game.print_board()
     my_game.get_move()
     my_game.check_for_winner()
@@ -23,7 +24,7 @@ if my_game.quit:
     print "Too bad. Play again soon!"
 else:
     my_game.print_board()
-    if my_game.winner != two_player.EMPTY:  # there is a winner
+    if my_game.winner != TicTacToe.EMPTY:  # there is a winner
         my_game.congratulate_winner()
     else:  # no winner: tie
         print "Tie!  Play again soon!"
